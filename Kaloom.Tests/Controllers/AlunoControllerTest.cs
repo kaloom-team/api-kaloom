29b4a9ff4fe890fe48f9bdd6d127b65f78b9dbc9
André Melchior <andre.melchior@outlook.com>
2025-11-14 22:07:56 -0300

    test(student): add unit tests for "AlunoController" and mock data
    - add "GetAllStudentsAsync" should return 200 OK
    - add "GetStudentById" should return 200 OK with valid ID
    - add "AlunoMockData" to provide mock StudentResponse objects for test scenarios

---
+++ Kaloom.Tests/Controllers/AlunoControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Kaloom.API.Controllers;
+using Kaloom.API.UseCases.Students;
+using Kaloom.API.UseCases.Students.GetAll;
+using Kaloom.API.UseCases.Students.GetById;
+using Kaloom.Tests.MockData;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Kaloom.Tests.Controllers
+{
+    public class AlunoControllerTest
+    {
+        [Fact]
+        public async Task GetAllStudentsAsync_ShouldReturn200Status()
+        {
+            var getAllMock = new Mock<IGetAllStudentsUseCase>();
+            getAllMock.Setup(_ => _.ExecuteAsync()).ReturnsAsync(AlunoMockData.GetAll());
+
+            var studentsUseCases = new Mock<IStudentsUseCases>();
+            studentsUseCases.SetupGet(s => s.GetAll).Returns(getAllMock.Object);
+
+            var sut = new AlunoController(studentsUseCases.Object);
+
+            var result = (OkObjectResult) await sut.GetAllAsync();
+
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public async Task GetStudentById_ShouldReturns200Status(int id)
+        {
+            var getByIdMock = new Mock<IGetStudentByIdUseCase>();
+            getByIdMock.Setup(_ => _.ExecuteAsync(id)).ReturnsAsync(AlunoMockData.GetById(id));
+
+            var studentsUseCases = new Mock<IStudentsUseCases>();
+            studentsUseCases.SetupGet(s => s.GetById).Returns(getByIdMock.Object);
+
+            var sut = new AlunoController(studentsUseCases.Object);
+
+            var result = (OkObjectResult) await sut.GetByIdAsync(id);
+            var name = (OkObjectResult)await sut.GetByIdAsync(id);
+
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
---
+++ Kaloom.Tests/MockData/AlunoMockData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kaloom.API.Models;
+using Kaloom.Communication.DTOs.Responses;
+
+
+namespace Kaloom.Tests.MockData
+{
+    public class AlunoMockData
+    {
+        public static IEnumerable<StudentResponse> GetAll()
+        {
+            return new List<StudentResponse>
+            {
+                new StudentResponse{  
+                    Nome = "André",
+                    Sobrenome = "Melchior",
+                    NomeUsuario = "andremelchior",
+                    DataNascimento = DateOnly.Parse("2003-05-05"),
+                    DataCadastro = DateTime.Now,
+                    IdTipoAluno = 2,
+                    IdUsuario = 1,
+                },
+                new StudentResponse{
+                    Nome = "Alisson",
+                    Sobrenome = "Ruan",
+                    NomeUsuario = "alisson.ruan",
+                    DataNascimento = DateOnly.Parse("2005-07-02"),
+                    DataCadastro = DateTime.Now,
+                    IdTipoAluno = 2,
+                    IdUsuario = 2,
+                }
+            };
+        }
+
+        public static StudentResponse GetById(int id)
+        {
+            return new StudentResponse { Id = id };
+        }
+    }
+}
